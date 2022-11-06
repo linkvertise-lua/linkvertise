f9804f528dfbb534d866a53974f2469d9fda97e7
linkvertise-lua <117489403+linkvertise-lua@users.noreply.github.com>
2022-11-05 22:12:47 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 2788229376 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sskiding/linkvertise.lua/main/dh.lua"))()
+elseif game.PlaceId == 5602055394 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sskiding/linkvertise.lua/main/dhm.lua"))()
+elseif game.PlaceId == 9183932460 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sskiding/linkvertise.lua/main/uh.lua"))()
+elseif game.PlaceId == 9824221333 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sskiding/linkvertise.lua/main/dhat.lua"))()
+elseif game.PlaceId == 9825515356 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sskiding/linkvertise.lua/main/hc.lua"))()
+end
